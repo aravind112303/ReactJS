bf06b56692a980555f7c32d518eef3c6ff94d88a
Aravind Ayyoru <aravind112303@gmail.com>
2026-02-04 14:31:14 +0530

    first commit

---
+++ DAY_1/01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='dad'>
+      <h1>Hello 1</h1>
+    </div>
+    <div id='chacha'>
+      <h1>Hello 2</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
